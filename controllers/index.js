3752b7b4b14a6a37977d2c420e63a766d04adb50
Garrett Watson <garrettbauer@gmail.com>
2020-02-01 04:01:43 +0900

    Complete readAll, readOne and test

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,9 @@ const { models } = require('../models');
 
 module.exports = {
   inventory: {
+
     create: async (req, res) => {
       const { name, description, price, quantity } = req.body;
-      console.log( req.body )
       await models.Inventory.create(
         {
           name,
@@ -15,34 +15,53 @@ module.exports = {
       ).catch((err) => res.status(500).send('Could not create record'))
       res.status(201).send('Record created');
     },
-    readAll: (req, res) => {
+
+    readAll: async (req, res) => {
+      const items = await models.Inventory.findAll()
+        .catch((err) => res.status(500).send('Could not read Inventory.'));
+      res.status(200).send(items);
+    },
+
+    readOne: async (req, res) => {
+      const { id } = req.params;
+      const item = await models.Inventory.findByPk(id).catch((err) => {
+        res.status(500).send('Error searching inventory.');
+      });
+      if (!item) {
+        res.status(400).send('Could not find item.')
+      } else {
+        res.status(200).send(item);
+      }
+    },
+
+    update: async (req, res) => {
 
     },
-    readOne: (req, res) => {
 
-    },
-    update: (req, res) => {
-
-    },
-    delete: (req, res) => {
+    delete: async (req, res) => {
 
     }
   },
 
   orders: {
-    create: (req, res) => {
+
+    create: async (req, res) => {
       const order = req.body;
     },
-    readAll: (req, res) => {
+
+    readAll: async (req, res) => {
 
     },
-    readOne: (req, res) => {
+
+    readOne: async (req, res) => {
 
     },
-    update: (req, res) => {
+
+    update: async (req, res) => {
 
     },
-    delete: (req, res) => {
+
+    delete: async (req, res) => {
 
     }
   },
