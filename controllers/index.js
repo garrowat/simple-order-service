8770c7342abc83032e45e224cf9937fa981f2d3e
Garrett Watson <garrettbauer@gmail.com>
2020-01-31 22:52:50 +0900

    Add separate model files for orders, inventory, also add test file for controllers index

--- controllers/index.js
+++ controllers/index.js
@@ -2,14 +2,38 @@ const models = require('../models');
 
 module.exports = {
   inventory: {
+    create: () => {
 
+    },
+    readAll: () => {
+
+    },
+    readOne: () => {
+
+    },
+    update: () => {
+
+    },
+    delete: () => {
+
+    }
   },
 
   orders: {
+    create: () => {
 
+    },
+    readAll: () => {
+
+    },
+    readOne: () => {
+
+    },
+    update: () => {
+
+    },
+    delete: () => {
+
+    }
   },
-
-  orderDetails: {
-
-  }
 };
---
+++ controllers/index.test.js
@@ -0,0 +1,7 @@
+const controllers = require('./');
+
+describe('Group', () => {
+  it('Should do something', () => {
+    expect(true).toBe(true);
+  })
+});
