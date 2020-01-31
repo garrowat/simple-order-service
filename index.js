10e81710e02f49210215fb2ea8738b59b48cec5e
Garrett Watson <garrettbauer@gmail.com>
2020-01-31 04:51:33 +0000

    Create route skeletons

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 const express = require('express');
-const app = express.app();
 
+const app = express();
 const PORT = process.env.PORT || '3000';
+
+app.post('/inventories', () => {
+
+});
+
+app.get('/inventories', () => {
+
+});
+
+app.get('/inventories/:id', () => {
+
+});
+
+app.put('/inventories/:id', () => {
+
+});
+
+app.delete('/inventories/:id', () => {
+
+});
+
+app.post('/orders', () => {
+
+});
+
+app.get('/orders', () => {
+
+});
+
+app.get('/orders/:id', () => {
+
+});
+
+app.put('/orders/:id', () => {
+
+});
+
+app.delete('/orders/:id', () => {
+
+});
 
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT}`);
